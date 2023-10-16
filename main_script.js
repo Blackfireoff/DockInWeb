3d30bbd7a95bb682bfbbba7d526c55fa71ae180d
BlackFire_off <96128400+Blackfireoff@users.noreply.github.com>
2023-10-16 21:48:18 +0200

    df

--- main_script.js
+++ main_script.js
@@ -1,19 +1,39 @@
 const { exec } = require('child_process');
-const express = require('express');
+const express = require('express';
+const crypto = require('crypto');
 
 const app = express();
 
 app.get('/create_container', (req, res) => {
-    exec('docker run -itd --name main_container debian', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Erreur lors de l'exécution de la commande : ${error}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.error(`stderr: ${stderr}`);
-        res.send('Conteneur créé avec succès');
-    });
+    const token = crypto.randomBytes(8).toString('hex'); // Génère un token aléatoire
+    const containerName = req.query.id ? req.query.id : `container_${token}`;
+    const containerExists = checkContainerExists(containerName);
+
+    if (containerExists) {
+        restartContainer(containerName);
+        res.send(`Redémarrage du conteneur ${containerName}`);
+    } else {
+        exec(`docker run -itd --name ${containerName} debian`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Erreur lors de l'exécution de la commande : ${error}`);
+                res.send('Une erreur s\'est produite lors de la création du conteneur');
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            console.error(`stderr: ${stderr}`);
+            res.send(`Conteneur ${containerName} créé avec succès`);
+        });
+    }
 });
+
+function checkContainerExists(containerName) {
+    const result = execSync(`docker ps -aqf "name=${containerName}"`);
+    return result.length > 0;
+}
+
+function restartContainer(containerName) {
+    exec(`docker restart ${containerName}`);
+}
 
 app.listen(3000, () => {
     console.log('Le serveur est en écoute sur le port 3000');
